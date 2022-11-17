5d8d21d2635e9150272daed36743d4894e8671e8
Manraj987 <114273127+Manraj987@users.noreply.github.com>
2022-11-16 20:08:10 -0800

    Update javascript.js

--- A5/javascript.js
+++ A5/javascript.js
@@ -34,9 +34,10 @@ function back(){
 } 
 
 var nextButton = document.getElementById("next"); 
-var previousButton = document.getElementById("previous"); {}
+var previousButton = document.getElementById("previous"); 
 
 previousButton.addEventListener("click",back,false);
+
 nextButton.addEventListener("click",next,false); 
 
 
