480acb252333d6704e1c3e4c277487b8fea956a7
Manraj987 <114273127+Manraj987@users.noreply.github.com>
2022-11-19 14:44:16 -0800

    Update Dot.js

--- Dot/Dot.js
+++ Dot/Dot.js
@@ -1,5 +1,5 @@
 
-  window.addEventListener("click", event => {
+/*  window.addEventListener("click", event => {
     var dot = document.createElement("div");
     dot.className = "dot";
     dot.style.left = (event.pageX - 4) + "px";
@@ -14,3 +14,27 @@ clear.addEventListener("click", function(CLEAR){
   CLEAR.stopPropagation();
  clear.addEventListener("click", refreshPage)
 })
+*/
+
+
+   
+var  mybutton = document.querySelector("button");
+mybutton.addEventListener("click", function(event) {
+
+
+var element = document.getElementsByTagName("div");
+for (index = element.length - 1; index >= 0; index--) {
+    element[index].parentNode.removeChild(element[index]);
+}
+
+// Let us stop the propagation of events
+
+event.stopPropagation();
+  });
+  addEventListener("click", function(event) {
+    var dot = document.createElement("div");
+    dot.className = "dot";
+    dot.style.left = (event.pageX - 4) + "px";
+    dot.style.top = (event.pageY - 4) + "px";
+    document.body.appendChild(dot);
+  });
